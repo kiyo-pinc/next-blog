7d674f432311d6b0fb0473dfcd6af6bd5bcd712d
清田 響介 <kiyota@hanabi-ya.jp>
2021-10-22 18:55:07 +0900

    カテゴリーのスタイル調整

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -14,9 +14,9 @@ type Props = ContainerProps
 const Component: React.VFC<Props> = ({ categories }) => (
   <div className={styles.wrapper}>
     <h1 className={styles.pageTitle}>カテゴリー</h1>
-    <ul className="m-0 p-0">
+    <ul className="p-0 my-0 mx-n2 d-flex flex-wrap">
       {categories.map((category) => (
-        <li key={category.id} className={styles.list}>
+        <li key={category.id} className={'px-2 ' + styles.list}>
           <Link href={pagesPath.category._categoryId(category.id).page._pageNumber(1).$url()}>
             <a>{category.name}</a>
           </Link>
