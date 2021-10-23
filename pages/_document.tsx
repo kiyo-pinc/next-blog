a4d0500c3d78c5eea1ef5e72c1101ab942e87415
Kiyota_K <kiyota@hanabi-ya.jp>
2021-10-23 21:08:43 +0900

    ファビコンの設定をしたよ

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,11 @@ class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <link rel="shortcut icon" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.png`} type="image/png" />
+          <link rel="shortcut icon" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`} type="image/png" />
+          <link rel="apple-touch-icon" sizes="180x180" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/apple-touch-icon.png`} />
+          <link rel="icon" type="image/png" sizes="32x32" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon-32x32.png`} />
+          <link rel="icon" type="image/png" sizes="16x16" href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon-16x16.png`} />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <Main />
